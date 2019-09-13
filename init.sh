87efe2d4ed3ad413b7201b30e47b86221e54756c
cor3000 <cor3000@web.de>
2019-09-13 20:25:11 +0800

    simplify init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,19 @@ echo ""
 echo "This script will assist you in setting up your custom ZK theme maven project."
 echo ""
 
+themeSrcPath=src/org/zkoss/theme
+themeTemplatePath=$themeSrcPath/___THEME_NAME___
+webAppInitFile=$themeTemplatePath/___THEME_NAME_CAP___ThemeWebAppInit.java
+
+templateFiles="pom.xml \
+readme.txt \
+package.json \
+preview/zk/example/ThemePreviewApp.java \
+${themeTemplatePath}/Version.java \
+${webAppInitFile} \
+src/archive/metainfo/zk/config.xml \
+src/archive/metainfo/zk/lang-addon.xml"
+
 # 1. gather information
 
 while true; do
@@ -25,9 +38,6 @@ while true; do
 		esac
 	done
 	
-	themeName=$artifactId
-	themeNameCap=$(echo $themeName | cut -c 1 | tr [a-z] [A-Z])$(echo $themeName | cut -c 2-)
-
 	while true; do
 		read -p "Enter the [VERSION] for your theme project and press [ENTER]: " version
 		case $version in
@@ -35,8 +45,6 @@ while true; do
 			* ) break;;
 		esac
 	done
-
-	
 
 	while true; do
 		read -p "Enter the [DISPLAY NAME] for your theme and press [ENTER]: " displayName
@@ -61,115 +69,41 @@ while true; do
 	esac
 done
 
-
-# 2. done gathering information, update the files now
-
-# need to update
-# 1. pom.xml
-# 2. folder path
-# 3. Version.java
-# 4. webAppInit.java
-# 5. config.xml
-# 6. lang-addon.xml
-
 set -e
 
-# first, check if files to be modified are present
+themeName=$artifactId
+themeNameCap=$(echo $themeName | sed 's/.*/\u&/g')
 
-if [ ! -e pom.xml ]; then
-	echo "[pom.xml] not found, unable to proceed, abort."
-	exit 2
-fi
-fileCount=$(find . -name "Version.java" | wc -l)
-if [ ! $fileCount -eq "1" ]; then
-	echo "more than one [Version.java], or file not found, unable to proceed, abort."
-	exit 2
-fi
-versionFilePath=$(find . -name "Version.java") # we now have the path to the file
-oldThemePath=${versionFilePath%/Version.java} # trim the file name
-oldThemeName=$(echo ${oldThemePath} | rev | cut -d / -f 1 | rev) # substring the theme name
-themeWebAppInitFileCount=$(find ${oldThemePath} -name "*ThemeWebAppInit.java" | wc -l)
-if [ ! $themeWebAppInitFileCount -eq "1" ]; then
-	echo "more than one [ThemeWebAppInit.java], or file not found, unable to proceed, abort."
-	exit 2
-fi
-themeWebAppInitFilePath=$(find ${oldThemePath} -name "*ThemeWebAppInit.java")
-if [ ! -e src/archive/metainfo/zk/config.xml ]; then
-	echo "[config.xml] not found, unable to proceed, abort."
-	exit 2
-fi
-if [ ! -e src/archive/metainfo/zk/lang-addon.xml ]; then
-	echo "[lang-addon.xml] not found, unable to proceed, abort."
-	exit 2
-fi
+# 2. check all expected files exist
+echo Checking files
+for file in $templateFiles; do
+    if [ ! -e $file ]; then
+	    echo "[$file] not found, unable to proceed, abort."
+	    exit 2
+    fi
+    echo Found: $file
+done
 
-# all files found, start updating
+echo All files found, start updating
+for file in $templateFiles; do
+    echo Updating: $file
+    sed -i.zktmp "s/___GROUP_ID___/${groupId}/g" ${file}
+    sed -i.zktmp "s/___ARTIFACT_ID___/${artifactId}/g" ${file}
+    sed -i.zktmp "s/___VERSION___/${version}/g" ${file}
+    sed -i.zktmp "s/___DISPLAY_NAME___/${displayName}/g" ${file}
+    sed -i.zktmp "s/___THEME_NAME___/${themeName}/g" ${file}
+    sed -i.zktmp "s/___THEME_NAME_CAP___/${themeNameCap}/g" ${file}
+done
 
-echo "updating pom.xml..."
-sed -i.zktmp "s/___GROUP_ID___/${groupId}/g" pom.xml
-sed -i.zktmp "s/___ARTIFACT_ID___/${artifactId}/g" pom.xml
-sed -i.zktmp "s/___VERSION___/${version}/g" pom.xml
-sed -i.zktmp "s/___DISPLAY_NAME___/${displayName}/g" pom.xml
-
-echo "updating Version.java..."
-sed -i.zktmp "s/___THEME_NAME___/${themeName}/g" ${versionFilePath}
-sed -i.zktmp "s/___VERSION___/${version}/g" ${versionFilePath}
-
-echo "updating ThemeWebAppInit.java..."
-sed -i.zktmp "s/___THEME_NAME___/${themeName}/g" ${themeWebAppInitFilePath}
-sed -i.zktmp "s/___THEME_NAME_CAP___/${themeNameCap}/g" ${themeWebAppInitFilePath}
-sed -i.zktmp "s/___DISPLAY_NAME___/${displayName}/g" ${themeWebAppInitFilePath}
-mv ${themeWebAppInitFilePath} ${oldThemePath}/${themeNameCap}ThemeWebAppInit.java
-
-echo "updating theme path..."
-mv ${oldThemePath} ${oldThemePath/${oldThemeName}/${themeName}}
-
-echo "updating config.xml..."
-configXmlFile=src/archive/metainfo/zk/config.xml
-sed -i.zktmp "s/___THEME_NAME___/${themeName}/g" ${configXmlFile}
-sed -i.zktmp "s/___THEME_NAME_CAP___/${themeNameCap}/g" ${configXmlFile}
-sed -i.zktmp "s/___VERSION___/${version}/g" ${configXmlFile}
-
-echo "updating lang-addon.xml..."
-langAddonFile=src/archive/metainfo/zk/lang-addon.xml
-sed -i.zktmp "s/___THEME_NAME___/${themeName}/g" ${langAddonFile}
-sed -i.zktmp "s/___VERSION___/${version}/g" ${langAddonFile}
-
-echo "updating readme.txt..."
-sed -i.zktmp "s/___THEME_NAME___/${themeName}/g" readme.txt
-
-echo "updating package.json..."
-sed -i.zktmp "s/___THEME_NAME___/${themeName}/g" package.json
-
-echo "updating ThemePreviewApp.java..."
-sed -i.zktmp "s/___THEME_NAME___/${themeName}/g" preview/zk/example/ThemePreviewApp.java
-
-echo "removing zktmp files..."
+echo "Removing tmp files..."
 find . -name "*.zktmp" | xargs rm -f
+
+echo Move files/folders 
+echo "  from: ${themeTemplatePath}"
+echo "    to: $themeSrcPath/$themeName"
+
+mv ${webAppInitFile} ${themeTemplatePath}/${themeNameCap}ThemeWebAppInit.java
+mv $themeTemplatePath/ $themeSrcPath/$themeName
 
 echo "All done."
 exit 0
-
-if [ "$OS" = "Windows_NT" ]; then # to avoid tons of if/else with uname -s in Windows
-	OS_detected="Windows"
-else
-	OS_detected=$(uname -s 2>/dev/null || echo not)
-fi
-
-case "$OS_detected" in
-	Darwin)
-		echo 'mac'
-	;;
-	
-	Linux)
-		echo 'linux'
-	;;
-
-	Windows)
-		echo 'windows'
-	;;
-
-	*)
-		echo 'other OS'
-	;;
-esac
