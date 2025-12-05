0067b5f97b63350fba36067cc13f63f3f211fad1
hawkchen <hawkchen@potix.com>
2025-12-05 14:12:52 +0800

    * fix some layout issues * put coachmark into a separate page

--- src/test/java/zk/example/ZulListVM.java
+++ src/test/java/zk/example/ZulListVM.java
@@ -7,7 +7,9 @@ import java.util.*;
 public class ZulListVM {
     public static final String HOME_PAGE = "preview.zul";
 
-    List<String> zulFiles = new ArrayList<>();
+    private List<String> zulFiles = new ArrayList<>();
+    //some pages will affect the preview rendering, it's better to preview it in a separate page
+    static private List<String> separatePageList = List.of("coachmark.zul");
 
     public void findZulFiles() {
         // Use getResource() to ensure correct path resolution in different environments (e.g., JAR, Java EE)
@@ -26,6 +28,9 @@ public class ZulListVM {
             }
         }
         Collections.sort(zulFiles);
+    }
+    static public boolean isSeparatePage(String zulPageName) {
+        return separatePageList.contains(zulPageName);
     }
 
     public ZulListVM() {
