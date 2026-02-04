dc00b4cda3fa725a1320018d5622ee43ac40a22a
James Chu <marchuqq@gmail.com>
2026-02-04 19:24:59 +0800

    Adjust WCAG Purple Compact

---
+++ src/main/java/org/zkoss/theme/wcag_purple_c/WCAGPurpleCompactThemeWebAppInit.java
@@ -0,0 +1,44 @@
+/* WCAGPurpleCompactThemeWebAppInit.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Jun 29, 2010 11:56:16 AM , Created by Sam
+
+Copyright (C) 2010 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under LGPL Version 3.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.theme.wcag_purple_c;
+
+import org.zkoss.zk.ui.WebApp;
+import org.zkoss.zk.ui.WebApps;
+import org.zkoss.zk.ui.util.WebAppInit;
+import org.zkoss.zkmax.theme.ResponsiveThemeRegistry;
+import org.zkoss.zul.theme.Themes;
+
+/**
+ * Initial the theme relative setting, includes
+ * Library property setting, Theme provider setting and Component definition setting 
+ * 
+ */
+public class WCAGPurpleCompactThemeWebAppInit implements WebAppInit {
+
+	private final static String THEME_NAME = "wcag_purple_c";
+	private final static String THEME_DISPLAY = "WCAG Purple Compact";
+	private final static int THEME_PRIORITY = 700;
+	
+	public void init(WebApp webapp) throws Exception {
+		Themes.register(THEME_NAME, THEME_DISPLAY, THEME_PRIORITY);
+		// Bug ZK-2963: register sapphire theme for tablet responsive theme
+		String edition = WebApps.getEdition();
+		if ("EE".equals(edition)) {
+			Themes.register(ResponsiveThemeRegistry.TABLET_PREFIX + THEME_NAME, THEME_DISPLAY, THEME_PRIORITY);
+		}
+	}
+}
--- src/test/java/zk/example/ThemePreviewApp.java
+++ src/test/java/zk/example/ThemePreviewApp.java
@@ -2,13 +2,20 @@ package zk.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.zkoss.lang.Library;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 @SpringBootApplication
+@Controller
 public class ThemePreviewApp {
     public static void main(String[] args) {
         System.setProperty("zk.homepage", "preview");
-        Library.setProperty("org.zkoss.theme.preferred", "wcag_navy_c");
+        Library.setProperty("org.zkoss.theme.preferred", "wcag_purple_c");
         //disable caches for development
         Library.setProperty("org.zkoss.zk.ZUML.cache", "false");
         Library.setProperty("org.zkoss.zk.WPD.cache", "false");
@@ -16,5 +23,11 @@ public class ThemePreviewApp {
         Library.setProperty("org.zkoss.web.classWebResource.cache", "false");
         Library.setProperty("org.zkoss.util.label.cache", "false");
         SpringApplication.run(ThemePreviewApp.class, args);
+    }
+
+    /** allow visiting each zul without zul e.g. http://localhost:8080/anchor */
+    @GetMapping("/{page}")
+    public String zulPage(@PathVariable("page") String page, HttpServletRequest request, HttpServletResponse response) {
+        return page ;
     }
 }
