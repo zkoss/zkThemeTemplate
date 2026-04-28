116b981dd2976cf735c45d406f9e82ef51336c9f
hawkchen <hawkchen@potix.com>
2026-04-28 17:19:49 +0800

    ajax navigation

---
+++ src/test/java/zk/example/UseCase2VM.java
@@ -0,0 +1,20 @@
+package zk.example;
+
+import org.zkoss.bind.annotation.BindingParam;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.NotifyChange;
+
+public class UseCase2VM {
+
+    private String currentPage = "~./usecase2/_default.zul";
+
+    public String getCurrentPage() {
+        return currentPage;
+    }
+
+    @Command
+    @NotifyChange("currentPage")
+    public void navigate(@BindingParam("page") String page) {
+        this.currentPage = page;
+    }
+}
