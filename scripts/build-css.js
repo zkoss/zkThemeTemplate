d4056655cefba0da471dd491b8f74b4bce6bd2e7
hawkchen <hawkchen@potix.com>
2026-05-04 18:26:39 +0800

    refine default.zul add chip

--- scripts/build-css.js
+++ scripts/build-css.js
@@ -17,6 +17,7 @@ const normFiles = [
     'zul/css/base/_reset.css',
     'zul/css/base/_utilities.css',
     'zul/css/base/_badges.css',
+    'zul/css/base/_chips.css',
     'zul/css/base/_avatars.css',
     'zul/css/base/_icons.css',
     'zul/css/zk-material.css',
